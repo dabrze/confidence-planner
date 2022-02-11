113727259be2d2ef4ba9df0489a3e9a72584f4b2
Lorakszak <70017011+Lorakszak@users.noreply.github.com>
2022-02-11 14:25:32 +0100

    Update colors

--- functions.py
+++ functions.py
@@ -1,9 +1,8 @@
 # Graph generation imports
 import plotly.graph_objects as go
 import plotly.express as px
-from itertools import cycle
 
-palette = cycle(px.colors.qualitative.Antique)
+palette = ['#cc6677', '#f6cf71', '#0f8554', '#1d6996', '#ff9900']
 
 def plot_confidence_interval(confidence_intervals, user_def):
     layout_ = go.Layout({"yaxis": {"title":"Confidence"},
@@ -17,13 +16,15 @@ def plot_confidence_interval(confidence_intervals, user_def):
     # fig.update_xaxes(range=[-3,103])
     fig.update_yaxes(showgrid=True, type='category')
 
+    col_number = 0
     for conf_int, category in zip(confidence_intervals, intervals):
         x = [round(conf_int[0], 2), round(conf_int[1], 2)]
         y = [category, category]
         fig.add_trace(go.Scatter(x=x, y=y, text=x,
                     mode='lines+markers+text',
                     textposition=['top center', 'top center'],
-                    line_color=next(palette),
+                    line_color=palette[col_number],
                     name=category))
+        col_number += 1
     #fig.show()
     return fig
