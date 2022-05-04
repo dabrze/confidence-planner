d773d1214fdd9003cb9dfc98c2e200e16aa6ddb6
AntKlo <80691856+AntKlo@users.noreply.github.com>
2022-05-04 21:50:15 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,33 +6,28 @@ with open("README.md", "r", encoding='UTF8') as fh:
 
 setuptools.setup(
     name="confidence-planner",
-    version="0.0.3",
+    version="0.0.1",
     author="Antoni Klorek, Karol Roszak",
     author_email="antoni.klorek@student.put.poznan.pl",
     description="Python package for calculating confidence interval for model's accuracy",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/Lorakszak/prediction-confidence-planner",
+    url="https://github.com/dabrze/confidence-planner",
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
-    python_requires=">=3.6",
+    python_requires=">=3.7",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         'Topic :: Software Development',
         'Topic :: Scientific/Engineering',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
     ],
     install_requires=[
-        "numpy>=1.17.0",
-        #"scikit-learn>=0.22",
-        #"pytest>=5.1.2",
-        #"coverage>=5.1",
-        #"pytest-cov>=2.8.1",
+        "numpy>=1.21.0",
         "scipy>=1.7.1",
         "statsmodels>=0.12.1"
     ]
